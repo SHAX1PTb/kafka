0a8685ea0bb6f3ecf2fc1bd440946b9fab28c9d8
SHAX1PTb03 <shankargouda.annigeri@capgemini.com>
2021-12-15 17:00:30 -0700

    initial

---
+++ src/main/java/test.java
@@ -0,0 +1,7 @@
+public class test {
+
+    //get kafka auto sych control manually
+    // them use after all processing commitSync - this will ensure all the work is compled and
+    // then issue commit, Else retry from previous commit.
+    //Take manual control for improving consistency and atomicity.
+}
