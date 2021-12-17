dff63c2617e7dc63fbefcd4aec1b03dbd1ed316a
SHAX1PTb03 <shankargouda.annigeri@capgemini.com>
2021-12-17 11:38:46 -0700

    Callback

--- src/main/java/com/example/kafka/Producer.java
+++ src/main/java/com/example/kafka/Producer.java
@@ -1,14 +1,17 @@
 package com.example.kafka;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class Producer {
+    final static Logger log = LoggerFactory.getLogger(Producer.class);
+
     public static void main(String[] args) {
+
 
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
@@ -17,8 +20,23 @@ public class Producer {
 
         final KafkaProducer producer = new KafkaProducer<String, String>(properties);
 
-        ProducerRecord<String, String> record = new ProducerRecord<>("TestTopic", "key1", "value1");
-        producer.send(record);
+        for (int i = 0; i < 10; i++) {
+
+            ProducerRecord<String, String> record = new ProducerRecord<>("TestTopic", "key_" + i, "value_" + i);
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        log.info("\nReceived Record Metadata::" + "\nTopic:" + recordMetadata.topic() + "\nPartation:" + recordMetadata.partition()
+                                + "\nOffset:" + recordMetadata.offset() + "\n@Timestamp:" + recordMetadata.timestamp());
+
+                    } else {
+                        log.error("Error Occurred", e);
+                    }
+                }
+            });
+        }
+
 
         producer.flush();
         producer.close();
