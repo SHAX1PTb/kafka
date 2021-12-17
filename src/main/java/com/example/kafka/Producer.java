1a02437b751fe6144a3e79776fe41b31118f44da
SHAX1PTb03 <shankargouda.annigeri@capgemini.com>
2021-12-17 10:04:12 -0700

    initial

---
+++ src/main/java/com/example/kafka/Producer.java
@@ -0,0 +1,28 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        final KafkaProducer producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("TestTopic", "key1", "value1");
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+
+
+    }
+}
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,4 +4,29 @@ public class test {
     // them use after all processing commitSync - this will ensure all the work is compled and
     // then issue commit, Else retry from previous commit.
     //Take manual control for improving consistency and atomicity.
+
+    private static test lazyInitialization = null;
+
+    public static void getInstance() {
+
+
+    }
+
+    private test test() {
+
+        if (lazyInitialization == null) {
+
+            synchronized (test.class) {
+
+                if (lazyInitialization == null) {
+
+                    lazyInitialization = new test();
+                }
+            }
+
+
+        }
+
+        return lazyInitialization;
+    }
 }
